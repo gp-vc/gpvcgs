1a253e2ab8d58f6aaa30399f5746f1fcbf31896f
keewook2 <keewook2@gmail.com>
2025-12-05 16:29:43 +0900

    added global layout and main page + contact us page from gpvc

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,9 @@
+import ContactWrapper from "@/src/components/sections/ContactWrapper";
+
+export default function Contact() {
+    return (
+        <div className='relative z-10'>
+            <ContactWrapper />
+        </div>
+    )
+}
